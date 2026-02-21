220905fa373c9ee47e18e42c222a2b87074dff13
DeinerGuerraDev <deinerdguerrap@gmail.com>
2026-02-21 14:32:08 -0500

    Los metodos que tiene que ver con la inscripción a la universidad se pasaron a este archivo

--- ProcesoDeIngresoUni/SingUp.cs
+++ ProcesoDeIngresoUni/SingUp.cs
@@ -1,11 +1,152 @@
 using System; 
-using System.Collections.Generic; 
+using System.Collections.Generic;
+using System.Reflection.Metadata;
 using System.Threading; 
 
-namespace ProcesodeIngresoUni
+
+namespace ProcesoIngreso
 {
-    public static class SingUp
+    public static class SingUpProcess
     {
-        
+        public static bool SingUp()
+        {
+            //Establecemos información para el usuario
+            Console.WriteLine("=============="); 
+            Console.WriteLine("Va a emepzar el proceso de inscripción...");
+            Thread.Sleep(3000); 
+            Console.WriteLine("==============");  
+
+            while (true)
+            {
+                //Establecemos entradas para el usuario
+                Console.Write("Ingresa tu Nombre Completo: "); 
+                Program.userName = Console.ReadLine(); 
+
+                Console.Write("Ingresa tu tarjeta de identidad: "); 
+                var userId = Console.ReadLine(); 
+
+                Console.Write("Ingresa tu tarjeta contraseña: "); 
+                string userPasswsword = Console.ReadLine(); 
+
+                //creamos una variable que almacena la cantidad de caracteres que tiene el userId
+                int cantidadNumerosId = userId.ToString().Length; 
+                
+
+                if (int.TryParse(userId, out int id)  && cantidadNumerosId <= 10)
+                {
+                    //Agregamos la infromación a la lista
+                    Program.UserPasswsword.Add(userPasswsword); 
+                    Program.UserName.Add(Program.userName);
+                    Program.UserId.Add(id);
+
+                    //agregamos algunos estados
+                    Program.IsAdmited.Add(false); 
+                    Program.IsAprobbed.Add(false); 
+                    Program.IsEnrolled.Add(false); 
+                    Program.IsRegister.Add(false);
+
+                    CargarDocuemntos(Program.userName, userPasswsword); 
+                    
+                    return Program.IsRegister.Contains(true);   
+                    
+
+                } 
+                else
+                {
+                    Console.WriteLine("Ingresa bien tu tarjeta de identidad!"); 
+                }
+            }
+
+
+        }
+        private static bool CargarDocuemntos(string userName, string userPassword)
+        {
+            while(true){
+                        
+                //Le decimos al estudiante que carge sus docuementos
+                Console.WriteLine("Ingrese los sigueintes docuemtos");
+                string[] documents = {"1. Recibo de pago de Matricula", "2. Pruebas Saber", "3. Formulario de Inscripción lleno"}; 
+                
+                //imprimimos las opciones
+                for (int i = 0; i < documents.Length; i++)
+                {
+                    Console.WriteLine(documents[i],"\t"); 
+                }
+                Thread.Sleep(3000); 
+
+                Console.WriteLine("Ingresa tu recibo de Matricula (pagada o no pagada)");
+                string userInvoice = Console.ReadLine().ToLower(); 
+
+                Console.WriteLine("Ingresa el resultado de tu prueba saber");
+                Program.userIcfes = Console.ReadLine().ToLower(); 
+
+                Console.WriteLine("Ingresa formulario (lleno o no completado)");
+                string userForm = Console.ReadLine().ToLower(); 
+
+                Thread.Sleep(3000); 
+
+                Console.WriteLine("\n==========================\n"); 
+                // Mostramos las carreras disponibles
+                Console.WriteLine("Ingresa una de las sigueintes carreras: \n");
+                Dictionary <string, List<string>> carreras = new Dictionary<string, List<string>>
+                    {
+
+                        {"Carreras de Ingenieria", new List<string> { "==========", "Ingenieria civil", "Ingenieria de Sistemas", "Ingenieria Ambiental", "Ingenieria Industrial", "Ingenieria Mecanica", "Arquitectura", "=========="}},
+                        {"Carreas de Ciencias Basicas", new List<string> { "==========", "Biología", "Quimica", "Arqueología", "=========="}}, 
+                        {"Carreras de Humanidades", new List<string> {"==========", "trabajo Social", "Psicología","Comunicación Social", "==========" }},
+
+                    }; 
+                
+                //Mostramos las facultades y las carreras disponibles
+                foreach(var facultad in carreras)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"Facultad: {facultad.Key}");
+                    foreach(var carrera in facultad.Value)
+                    {
+                        Console.ForegroundColor= ConsoleColor.DarkGray;
+                        Console.WriteLine($"- {carrera}"); 
+                    } 
+                }
+
+                Console.WriteLine("\n==========================\n"); 
+
+                //le pedimos al usuario que ingrese una carrera
+                Console.Write("Ingresa una facultad: "); 
+                Program.userFaculty = Console.ReadLine(); 
+
+                Console.Write("\nIngresar una carrera: ");
+                Program.userCarrer = Console.ReadLine(); 
+
+                Console.WriteLine("\n======================\n");
+                //Verificamos que el usuario ingrese los docuemntos
+                if (userInvoice == "pagada" && userForm == "lleno" && Program.userIcfes != null)
+                    {
+                        Console.WriteLine("Completaste tu registro!\n");
+                        Console.Write($"Tú usario es: ");
+
+                        Console.ForegroundColor = ConsoleColor.DarkGreen; 
+                        Console.Write(userName.ToUpper());
+
+                        Console.ResetColor();  
+                        Console.Write("tu contraseña es: "); 
+
+                        Console.ForegroundColor = ConsoleColor.DarkGreen; 
+                        Console.Write(userPassword.ToUpper()); 
+
+                        Program.UserBasicInfo.Add(userName, new List<string> {userForm, Program.userIcfes, userInvoice, Program.userFaculty, Program.userCarrer});
+                        return true;   
+                    }   
+                else
+                {
+                    Console.WriteLine("Debes de agregar todos los docuemtos"); 
+                }
+
+                    
+                
+            }
+
+
+        }  
     }
 }
