76de88b51e1de24f282b7dfa3db19081e08fa6cb
DeinerGuerraDev <deinerdguerrap@gmail.com>
2026-02-21 21:53:15 -0500

    Se agrego la parte de salida  y Admi

--- ProcesoDeIngresoUni/InputManagment.cs
+++ ProcesoDeIngresoUni/InputManagment.cs
@@ -9,43 +9,54 @@ namespace ProcesoIngreso
     {
         public static void ProcesarAccion(int accion)
         {
-            switch (accion)
-            {
-                case 1: 
-                    Register.SingUp();  
-                    Console.WriteLine("Acción Exitosa, espere un momento..."); 
-                    Thread.Sleep(3000); 
-                    break; 
-                case 2: 
+            while(true){
+                switch (accion)
                 {
-                    Result.AdmissionResult(); 
-                    Console.WriteLine("Acción Exitosa, espere un momento..."); 
-                    Thread.Sleep(3000); 
-                    break; 
+                    case 1: 
+                        Register.SingUp();  
+                        Console.WriteLine("Acción Exitosa, espere un momento..."); 
+                        Upload.Motion(); 
+                        break; 
+                    case 2: 
+                    {
+                        Result.AdmissionResult(); 
+                        Console.WriteLine("Acción Exitosa, espere un momento..."); 
+                        Upload.Motion();  
+                        break; 
+                    }
+                    case 3: 
+                        Test.TestProgram();
+                        Console.WriteLine("Acción Exitosa, espere un momento..."); 
+                        Upload.Motion(); 
+                        break; 
+                    case 4: 
+                        Test.DisplayResult();
+                        Console.WriteLine("Acción Exitosa, espere un momento..."); 
+                        Upload.Motion(); 
+                        break; 
+                    case 5: 
+                        Console.WriteLine("subir docuemtos para la matricula");
+                        Console.WriteLine("Acción Exitosa, espere un momento..."); 
+                        Upload.Motion(); 
+                        break; 
+                    case 6: 
+                        Console.WriteLine("Revisar estado de Matricula");
+                        Console.WriteLine("Acción Exitosa, espere un momento..."); 
+                        Upload.Motion(); 
+                        break;  
+                    case 7: 
+                        AdminFunctions.SingUp(); 
+                        break; 
+                    case 8: 
+                        Console.ForegroundColor = ConsoleColor.Red; 
+                        Console.WriteLine(); 
+                        Console.ResetColor(); 
+                        break; 
+                    default: 
+                        Console.WriteLine("Ingresa una entrada valida");
+                        Upload.Motion(); 
+                        break;         
                 }
-                case 3: 
-                    Test.TestProgram();
-                    Console.WriteLine("Acción Exitosa, espere un momento..."); 
-                    Thread.Sleep(3000); 
-                    break; 
-                case 4: 
-                    Test.DisplayResult();
-                    Console.WriteLine("Acción Exitosa, espere un momento..."); 
-                    Thread.Sleep(3000); 
-                    break; 
-                case 5: 
-                    Console.WriteLine("subir docuemtos para la matricula");
-                    Console.WriteLine("Acción Exitosa, espere un momento..."); 
-                    Thread.Sleep(3000); 
-                    break; 
-                case 6: 
-                    Console.WriteLine("Revisar estado de Matricula");
-                    Console.WriteLine("Acción Exitosa, espere un momento..."); 
-                    Thread.Sleep(3000); 
-                    break;  
-                default: 
-                    Console.WriteLine("Ingresa una entrada valida");
-                    break;         
             }
         }
     }
