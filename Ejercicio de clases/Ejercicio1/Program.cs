0cfa1f0fdfe7cc1ccf9f8474986a33a5558470a6
DeinerGuerraDev <deinerdguerrap@gmail.com>
2026-02-25 07:17:55 -0500

    se desarrollo un algoritmo para saber cuales números son pares

---
+++ Ejercicio de clases/Ejercicio1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System; 
+using System.Collections.Generic; 
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            while(true)
+            {
+                Console.Write("Ingrese un número: ");
+                int numero = Convert.ToInt16(Console.ReadLine()); 
+
+                if (numero % 2 == 0)
+                {
+                    Console.WriteLine("El número es Par"); 
+                }
+                else
+                {
+                    Console.WriteLine("el número no es par");
+                }
+                Console.WriteLine("Salir (s/n)");
+                string respuesta = Console.ReadLine().ToLower(); 
+
+                if (respuesta == "s")
+                {
+                    Console.WriteLine("saliendo de la aplicación"); 
+                    break; 
+                }
+                
+            }
+        }
+    }
+}
